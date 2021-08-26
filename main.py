c86980209a88a2f06e223b4946cb4e08b1b05aa3
T-UwU <72111629+T-UwU@users.noreply.github.com>
2021-08-26 15:23:14 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,20 @@
+def on_forever():
+    pass
+basic.forever(on_forever)
+
+Act_1 = 0
+basic.show_string("A1=")
+basic.show_number(Act_1)
+Act_2 = 0
+basic.show_string("A2=")
+basic.show_number(Act_2)
+Act_3 = 0
+basic.show_string("A3=")
+basic.show_number(Act_3)
+Examen = 5
+basic.show_string("E=")
+basic.show_number(Examen)
+Quiz = 0
+basic.show_string("Q=")
+basic.show_number(Quiz)
+basic.clear_screen()
---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function on_forever() {
+    
+})
+let Act_1 = 0
+basic.showString("A1=")
+basic.showNumber(Act_1)
+let Act_2 = 0
+basic.showString("A2=")
+basic.showNumber(Act_2)
+let Act_3 = 0
+basic.showString("A3=")
+basic.showNumber(Act_3)
+let Examen = 5
+basic.showString("E=")
+basic.showNumber(Examen)
+let Quiz = 0
+basic.showString("Q=")
+basic.showNumber(Quiz)
+basic.clearScreen()
